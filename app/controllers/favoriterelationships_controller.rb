3b2e79c347a93e2e211fc51da0c980ca6d17e822
takuro.sonobe <tkr.ld.not.nyd@gmail.com>
2018-11-13 11:22:12 +0000

    kadai-favorites

---
+++ app/controllers/favoriterelationships_controller.rb
@@ -0,0 +1,17 @@
+class FavoriterelationshipsController < ApplicationController
+  before_action :require_user_logged_in
+  
+  def create
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.favorite(micropost)
+    flash[:success] = 'Micropostをお気に入りに追加しました'
+    redirect_to current_user
+  end
+
+  def destroy
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unfavorite(micropost)
+    flash[:success] = 'Micropostをお気に入りから解除しました'
+    redirect_to current_user
+  end
+end
